98d499b4f39b9a637a16bdfe5facd308ef776132
ijgnd <fooijgnd@barijgnd>
2019-12-22 11:54:26 +0100

    narrow bar for add-on "browser side-by-side"

--- src/infobar.py
+++ src/infobar.py
@@ -16,12 +16,65 @@ def gc(arg,fail=False):
     return mw.addonManager.getConfig(__name__).get(arg,fail)
 
 
+def editor_by_the_side(self):
+    # better compatibility with "browser side-by-side (horizontal split)"
+    # https://ankiweb.net/shared/info/831846358
+    try:
+        if self.side_by_side:
+            return True
+        else:
+            return False
+    except:
+        return False
+Browser.editor_by_the_side = editor_by_the_side
+
+
 def addInfoBar(self):
+    if self.editor_by_the_side():
+        self.addInfoBar_narrow()
+    else:
+        self.addInfoBar_default()
+Browser.addInfoBar = addInfoBar
+
+
+def addInfoBar_narrow(self):
+    a = ["dates", "dueIvlEase", "RvsLpsAvtime", "cidNid", "cardtypeOrd", "noteType", "Deck"]
+    for i in a:
+        setattr(self,"il_" + i, QLabel(self))
+        setattr(self,"i_" + i, QLabel(self))
+
+    g = [
+        #       0                1            2  3  4  5      6                7   8  9 10 11
+        [self.il_dates,       'Ad/FR/LR: ',   0, 0, 1, 1, self.i_dates,        "", 0, 1, 1, 1],
+        [self.il_dueIvlEase,  'Du/Iv/Es: ',   1, 0, 1, 1, self.i_dueIvlEase,   "", 1, 1, 1, 1],
+        [self.il_RvsLpsAvtime,'Rv/Lp/AvTi: ', 2, 0, 1, 1, self.i_RvsLpsAvtime, "", 2, 1, 1, 1],
+        [self.il_cidNid,      'cid/nid: ',    3, 0, 1, 1, self.i_cidNid,       "", 3, 1, 1, 1],
+        [self.il_cardtypeOrd, 'Ctyp/Tmpl: ',  4, 0, 1, 1, self.i_cardtypeOrd,  "", 4, 1, 1, 1],
+        [self.il_noteType,    'Notetype: ',   5, 0, 1, 1, self.i_noteType,     "", 5, 1, 1, 1],
+        [self.il_Deck,        'Deck: ',       6, 0, 1, 1, self.i_Deck,         "", 6, 1, 1, 1],
+    ]
+
+    while self.form.infogrid.count():
+        item = self.form.infogrid.takeAt(0)
+        widget = item.widget()
+        widget.deleteLater()
+
+    for l in g:
+        t = "<b>" + l[1] + "</b>"  # increases height noticeably
+        l[0].setText(t) 
+        #l[0].setStyleSheet('background-color: rgb(100, 10, 1);')
+        l[6].setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        self.form.infogrid.addWidget(l[0], l[2], l[3], l[4], l[5])
+        self.form.infogrid.addWidget(l[6], l[8], l[9], l[10], l[11])
+Browser.addInfoBar_narrow = addInfoBar_narrow
+
+
+def addInfoBar_default(self):
     a = ["added","fr","lr","due","ivl","ease","revs","laps","avTime",
             "cardType","noteType","Deck", "nid", "cid"] 
     for i in a:
-        setattr(self,"il_" + i,QLabel(self))
-        setattr(self,"i_" + i,QLabel(self))
+        setattr(self,"il_" + i, QLabel(self))
+        setattr(self,"i_" + i, QLabel(self))
 
     g = [
         #      0                1           2  3  4  5      6            7   8  9 10 11
@@ -39,6 +92,11 @@ def addInfoBar(self):
         [self.il_Deck,     'Deck:      ',   2, 6, 1, 1, self.i_Deck,     "", 2, 7, 1, 2],
         [self.il_avTime,   'AvgTime: ',     0, 8, 1, 1, self.i_avTime,   "", 0, 9, 1, 1],
     ]
+
+    while self.form.infogrid.count():
+        item = self.form.infogrid.takeAt(0)
+        widget = item.widget()
+        widget.deleteLater()
 
     for l in g:
         t = "<b>" + l[1] + "</b>"  # increaes height noticeable
@@ -58,25 +116,52 @@ def addInfoBar(self):
     self.form.infogrid.setColumnStretch(7,4)
     self.form.infogrid.setColumnStretch(8,1)
     self.form.infogrid.setColumnStretch(9,2)
+Browser.addInfoBar_default = addInfoBar_default
 
 
-def update(self):
+def updateInfoBar(self):
+    if self.editor_by_the_side():
+        updateInfoBar_narrow(self)
+    else:
+        updateInfoBar_default(self)
+Browser.updateInfoBar = updateInfoBar
+
+
+def updateInfoBar_narrow(self):
     if self.card:
         p = self.cardstats(self.card)
-        self.i_added.setText(p.Added) 
-        self.i_fr.setText(p.FirstReview) 
-        self.i_lr.setText(p.LatestReview)
-        self.i_due.setText(p.Due)
-        self.i_ivl.setText(p.Interval)
-        self.i_ease.setText(p.Ease)
-        self.i_revs.setText(p.Reviews + "/ " + p.Lapses)
-        self.i_avTime.setText(p.AverageTime)
-        self.i_cardType.setText(p.CardType)
-        self.i_noteType.setText(p.NoteType)
-        self.i_Deck.setText(p.Deck)
-        self.i_nid.setText(p.NoteID)
-        self.i_cid.setText(p.CardID)
-            
+        try:
+            self.i_dates.setText("{}/{}/{}".format(p.Added, p.FirstReview, p.LatestReview))  # Added/FirRev/LatRev
+            self.i_dueIvlEase.setText("{}/{}/{}".format(p.Due, p.Interval, p.Ease))  # Due/Ivl/Ease
+            self.i_RvsLpsAvtime.setText("{}/{}/{}".format(p.Reviews, p.Lapses, p.AverageTime))  # Rvs/Lps/AvgTime
+            self.i_cidNid.setText("{}/{}".format(p.CardID, p.NoteID))  # cid/nid
+            self.i_cardtypeOrd.setText("{}/{}".format(p.CardType, p.CardOrd))  # CardType/TmplNo
+            self.i_noteType.setText(p.NoteType)   # NoteType
+            self.i_Deck.setText(p.Deck)  # Deck
+        except:
+            pass
+
+
+def updateInfoBar_default(self):
+    if self.card:
+        p = self.cardstats(self.card)
+        try:
+            self.i_added.setText(p.Added) 
+            self.i_fr.setText(p.FirstReview) 
+            self.i_lr.setText(p.LatestReview)
+            self.i_due.setText(p.Due)
+            self.i_ivl.setText(p.Interval)
+            self.i_ease.setText(p.Ease)
+            self.i_revs.setText(p.Reviews + " / " + p.Lapses)
+            self.i_avTime.setText(p.AverageTime)
+            self.i_cardType.setText(p.CardType)
+            self.i_noteType.setText(p.NoteType)
+            self.i_Deck.setText(p.Deck)
+            self.i_nid.setText(p.NoteID)
+            self.i_cid.setText(p.CardID)
+        except:
+            pass
+
 
 def setupUi(self, Dialog):
     self.verticalLayout_2.removeWidget(self.tableView)
@@ -90,15 +175,16 @@ def setupUi(self, Dialog):
 
 
 Ui_Dialog.setupUi = wrap(Ui_Dialog.setupUi, setupUi)
-addHook("browser.setupMenus", addInfoBar)
-addHook("browser.rowChanged", update)
+#addHook("browser.setupMenus", addInfoBar)
+addHook("browser.rowChanged", updateInfoBar)
 Browser.cardstats = cardstats
-
 
 
 def toggle_infobox(self):
     if not self.form.infowidget.isVisible():
         self.form.infowidget.setVisible(True)
+        self.addInfoBar()
+        self.updateInfoBar()
     else:
         self.form.infowidget.setVisible(False)
 Browser.toggle_infobox = toggle_infobox
